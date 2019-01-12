9a4ebc4f42c9dbede12cccd1f17e0db1993bb68b
Dahiya <adahiy3@LAMU02XHLYCJG5J.uhc.com>
2019-01-12 17:31:42 -0600

    first commit for project test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ArrayList<String> al = new ArrayList<>();
+
+        al.add("amit");
+        al.add("anju");
+
+        System.out.println("value of list is" + al);
+
+    }
+}
